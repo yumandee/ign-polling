88f736c93bd26d4027edbecf33712338350b131a
yumandee <yumandee@yahoo.com>
2022-04-30 18:59:04 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: {
-    MONGO_URI: process.env.MONGO_URI,
-  }
-}
+    MONGO_URI:
+      "mongodb+srv://codefoo2022:yOSPD6ZhAzzS0dUL@poll-cluster.ihjif.mongodb.net/polling-db?retryWrites=true&w=majority",
+  },
+};
