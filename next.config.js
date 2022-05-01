896c09bfcdcd84c2a58c7f6d2b2d600ec1af7251
Mandy Yu <yumandee@yahoo.com>
2022-04-30 20:24:33 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
     MONGO_URI:
-      "mongodb+srv://codefoo2022:yOSPD6ZhAzzS0dUL@poll-cluster.ihjif.mongodb.net/polling-db?retryWrites=true&w=majority",
+      "mongodb+srv://codefoo2022:kA0MP9rEhxMDoZg1@poll-cluster.ihjif.mongodb.net/polling-db?retryWrites=true&w=majority",
   },
 };
